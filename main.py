53d478188783af2b9fd07b37a56d5f45ac44df76
KulshinTony <KulshinTony@gmail.com>
2024-08-03 19:10:15 +0700

    add stay_a_live

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import asyncio
 import os
 
 from utils.handlers import router
+from utils.stay_a_live import stay_a_live
 
 
 async def main():
     bot = Bot(token=os.getenv("Betta_Token"))
     dp = Dispatcher()
     dp.include_router(router)
+    stay_a_live()
 
     await dp.start_polling(bot)
 
